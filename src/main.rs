6191e3a9d4a089239b05d5a945918812e04b53ef
Zack Norton <norton.zack.w@gmail.com>
2023-12-14 04:08:37 +0000

    set timers to 25 and 5

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,25 @@ fn main() {
     println!("Pomodoro Timer");
     println!("Begin work");
 
-    let stop_time = SystemTime::now().add(Duration::from_secs(5));
+    let stop_time = SystemTime::now()
+        .add(
+            Duration::from_secs(
+                60 * 25 //1500 seconds = 25 minutes
+            )
+        );
 
     while SystemTime::now() < stop_time {}
 
     println!("Break time!");
 
+    let stop_time = SystemTime::now()
+        .add(
+            Duration::from_secs(
+                5 * 60 //300 seconds == 5 minutes
+            )
+        ); 
+
+    while SystemTime::now() < stop_time {}
+
+    println!("Break time over...");
 }
