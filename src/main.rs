7a80ec3d03a8b895cfbc21c03fa674d35359f8a7
Zack Norton <norton.zack.w@gmail.com>
2023-12-15 03:58:15 +0000

    pomo dev

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
 use std::time::{Duration, SystemTime};
+use std::io;
 use std::ops::Add;
 
+struct Pomodoro {
+    is_running: bool
+}
+
+impl Pomodoro {
+    fn new() -> Self {
+        return Pomodoro {
+            is_running : false
+        }
+    }
+}
+
 fn main() {
+
+    let mut pomo = Pomodoro::new();
+    
+    loop {
+        println!("Enter command");
+
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).expect("failed to read line");
+
+        match command.as_str() {
+            "pomodoro" => {
+                if !pomo.is_running {
+                    pomo.is_running = true;
+                    start_pomodoro_timer()
+                }
+            },
+            "exit" => break,
+            _ => {
+                println!("Unknown command");
+            }
+        }
+    }
+
+    println!("Exiting...");
+}
+
+fn start_pomodoro_timer() {
     println!("Pomodoro Timer");
     println!("Begin work");
 
